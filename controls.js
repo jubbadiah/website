9f498672f9e8b054faa2142f1934ebf4573313e8
unknown <luigi_334@hotmail.com>
2017-11-24 21:05:21 +0000

    audio buttons, borders.

--- controls.js
+++ controls.js
@@ -1,15 +1,37 @@
-var myTrack = document.getElementById('mytrack');
-var playButton = document.getElementById('playButton');
+// var myTrack = document.getElementByClass('mytrack');
+// var playButton = document.getElementsByClass('playButton');
 
-playButton.addEventListener('click', playOrPause, false);
+const wrappers = document.querySelectorAll('.wrapper');
 
-function playOrPause() {
-    if (!myTrack.paused && !myTrack.ended) {
-        myTrack.pause();
-        playButton.style.backgroundImage = 'url(./play.png)';
+wrappers.forEach(wrapper => {
+    const playButton = wrapper.getElementsByClassName('playButton')[0];
+    const myTrack = wrapper.getElementsByClassName('mytrack')[0];
+
+    playButton.addEventListener('click', playOrPause, false);
+
+    function playOrPause() {
+        if (!myTrack.paused && !myTrack.ended) {
+            myTrack.pause();
+            playButton.style.backgroundImage = 'url(./play.png)';
+        }
+        else {
+            myTrack.play();
+            playButton.style.backgroundImage = 'url(./pause.png)';
+        }
     }
-    else {
-        myTrack.play();
-        playButton.style.backgroundImage = 'url(./pause.png)';
-    }
-}
+});
+
+
+
+// playButton.addEventListener('click', playOrPause, false);
+
+// function playOrPause() {
+//     if (!myTrack.paused && !myTrack.ended) {
+//         myTrack.pause();
+//         playButton.style.backgroundImage = 'url(./play.png)';
+//     }
+//     else {
+//         myTrack.play();
+//         playButton.style.backgroundImage = 'url(./pause.png)';
+//     }
+// }
